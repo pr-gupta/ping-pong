4f057cac3df22330468351700644bba72b83fe90
Prashant Gupta <pr.gu1996@gmail.com>
2022-07-08 12:49:11 +0530

    Add completed code.

---
+++ first_complete.py
@@ -0,0 +1,38 @@
+from helper import *
+
+wn = setup_screen()
+
+# ------------------------------------------- #
+# --------------- START CODE ---------------- #
+# ------------------------------------------- #
+
+# draw a ball
+
+ball = turtle.Turtle()
+ball.shape("circle")
+ball.color("red")
+
+# draw 1st paddle
+
+paddle1 = turtle.Turtle()
+paddle1.shape("rectangle")
+paddle1.color("red")
+paddle1.penup()
+paddle1.goto(-350, 0)
+
+# draw 2nd paddle
+
+paddle2 = turtle.Turtle()
+paddle2.shape("rectangle")
+paddle2.color("red")
+paddle2.penup()
+paddle2.goto(350, 0)
+
+# ------------------------------------------- #
+# --------------- END CODE ------------------ #
+# ------------------------------------------- #
+
+wn.update()
+turtle.Screen().exitonclick()
+
+# ref - http://christianthompson.com/
